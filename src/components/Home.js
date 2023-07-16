b855e5610e76c225c6bc3343b20379eb781d2cc7
NaserGharbieh <naserm.gharbieh@gmail.com>
2023-07-16 20:33:18 +0000

    class18

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieList from './MovieList';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+         <MovieList/>
+            
+        </div>
+    );
+}
+
+export default Home;
