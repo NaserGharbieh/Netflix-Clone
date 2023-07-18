8bd6be688f812d269f34ee0fd123927669fde9e7
NaserGharbieh <naserm.gharbieh@gmail.com>
2023-07-18 19:40:42 +0000

    Lab19?!

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import MovieList from './MovieList';
+import MovieList from './MovieList'; 
+import Navbar from './Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar/>
             <h2>Home</h2>
          <MovieList/>
             
